a751caf22d40e626c2e814c78030ff18260b8fe3
nicole-santana <nicole.teles.santana@gmail.com>
2023-08-27 18:34:45 -0300

    Título e um pouco sobre mim adicionado

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let nome = "Nicole"
+let greet = "Olá, meu nome é"
+let greettwo = ", e esse é o meu primeiro projeto sobre Javascript fora do meu curso técnico"
+
+const mensagem =  greet + " " + nome + " " + greettwo
+let mensagemAtual = mensagem + "❤️"
+
+document.getElementById("greeting").innerText = mensagem
+
+
+
 let cliques = 0
 
 function adicionar(){
@@ -7,10 +18,7 @@ function adicionar(){
 }
 
 function salvar() {
-    console.log(cliques)
+    let hist = cliques
+    document.getElementById("historico").innerText= hist
 }
 
-let username ="nick"
-let message="new!"
-
-console.log(username,"vc tem uma notificação", message)
