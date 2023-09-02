5e67200d3506c7aa5a0c5232a6d6e8f049dfe850
nicole-santana <nicole.teles.santana@gmail.com>
2023-09-01 22:08:03 -0300

    mudanças na parte do histórico

---
+++ exercicios.js
@@ -0,0 +1,12 @@
+//exercício 6(?)
+
+let nome = "Nicole"
+let greet = "Olá, meu nome é"
+let greettwo = ", e esse é o meu primeiro projeto sobre Javascript fora do meu curso técnico"
+
+let mensagem =  greet + " " + nome + " " + greettwo
+mensagem += "❤️"
+
+document.getElementById("greeting").innerText = mensagem
+
+
--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-let nome = "Nicole"
-let greet = "Olá, meu nome é"
-let greettwo = ", e esse é o meu primeiro projeto sobre Javascript fora do meu curso técnico"
-
-const mensagem =  greet + " " + nome + " " + greettwo
-let mensagemAtual = mensagem + "❤️"
-
-document.getElementById("greeting").innerText = mensagem
-
-
 
 let cliques = 0
 
 function adicionar(){
-    cliques= cliques+1
+    //não prescisa colocar '+1', ele funciona sem
+    cliques+= 1
     document.getElementById("count-el").innerText=cliques
     
 }
@@ -22,3 +13,7 @@ function salvar() {
     document.getElementById("historico").innerText= hist
 }
 
+
+//Resultados passados
+
+let entradas = document.getElementById("entradas")
